7e603790e6c0d53b50519b5ca29d33424c37815a
Marquinhos <fernandez9029@gmail.com>
2026-04-13 12:08:59 -0500

    primer commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Landscape} from "@/components/Landscape";
+
+
+const page = () => {
+  return (
+    <div className={`h-screen w-full text-slate-200 bg-radial-[at_50%_50%] from-gray-700 to-slate-950 flex flex-col justify-between items-center`}>
+      <h1 className={` text-5xl`}>Site under construction</h1>
+      <Landscape/>
+    </div>
+  );
+};
+export default page;
